692abcdf1a649c4aec02374fa668b5e686da997e
saydaliyev elmurod <saydaliyevelmurod01@gmail.com>
2024-05-16 10:09:10 +0500

    day solution

---
+++ src/leetcode75/daySolutions/may16/Solution.java
@@ -0,0 +1,32 @@
+package leetcode75.daySolutions.may16;
+
+import leetcode75.tree.levelOrder.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    //https://leetcode.com/problems/evaluate-boolean-binary-tree/description/
+    public boolean evaluateTree(TreeNode root) {
+        if (root.val == 1 || root.val == 0) {
+            return root.val == 1;
+        }
+        if (root.val == 2) {
+            return evaluateTree(root.left) || evaluateTree(root.right);
+        } else {
+            return evaluateTree(root.left) && evaluateTree(root.right);
+        }
+    }
+}
